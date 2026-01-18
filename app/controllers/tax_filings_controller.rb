20052b1081e7d2c34f4094bfa781e8b297d27f79
mercklyzer <mercklyzer@gmail.com>
2026-01-18 17:34:58 +0800

    feat: add GET companies/:id path

--- app/controllers/tax_filings_controller.rb
+++ app/controllers/tax_filings_controller.rb
@@ -27,4 +27,39 @@ class TaxFilingsController < ApplicationController
       }
     })
   end
+
+  def show
+    # Assumption: There can be multiple tax filings under the same EIN for different years.
+    tax_filings = TaxFiling.where(ein: params[:id]).order(tax_year: :desc)
+
+    if tax_filings.nil?
+      render_json({ error: "Tax filing not found" }, status: :not_found)
+      return
+    end
+
+    returned_tax_filings = tax_filings.map do |filing|
+      filing.as_json.merge({
+        # need to convert to float as big decimals are returned as strings in JSON in Rails
+        revenue_delta_amount: filing.revenue_delta_amount.to_f,
+        revenue_delta_percent: filing.revenue_delta_percent.to_f,
+        expenses_delta_amount: filing.expenses_delta_amount.to_f,
+        expenses_delta_percent: filing.expenses_delta_percent.to_f,
+        assets_delta_amount: filing.assets_delta_amount.to_f,
+        assets_delta_percent: filing.assets_delta_percent.to_f,
+        employees_delta_amount: filing.employees_delta_amount.to_f,
+        employees_delta_percent: filing.employees_delta_percent.to_f
+
+        # I decided to not display additional fields such as
+        # key personnel full names, and their titles,
+        # expenses by category,
+        # revenue vs expense trend
+        # and staffing signal
+        # due to lack of time.
+      })
+    end
+
+    render_json({
+      data: returned_tax_filings
+    })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :companies, only: :index, controller: "tax_filings"
+  resources :companies, only: [:index, :show], controller: "tax_filings"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
